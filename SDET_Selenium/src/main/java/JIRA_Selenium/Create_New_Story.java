f7454ec2e0bfbb543cf8b046d7d0b477355ff060
tamilelavenil <93430431+tamilelavenil@users.noreply.github.com>
2023-09-09 15:47:03 +0530

    Update Create_New_Story.java

--- SDET_Selenium/src/main/java/JIRA_Selenium/Create_New_Story.java
+++ SDET_Selenium/src/main/java/JIRA_Selenium/Create_New_Story.java
@@ -1,6 +1,5 @@
 package JIRA_Selenium;
 import java.time.Duration;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;

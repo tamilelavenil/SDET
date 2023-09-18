52705a66930646c01d558da315048fbef58a3498
tamilelavenil <tamilelavenil@gmail.com>
2023-09-19 00:07:15 +0530

    SubstringofSizeThreewithDistinctCharacters

--- SDET_DSA/src/main/java/mandatoryHomework/week1/TwoPointer_SlidingWindow/SubstringofSizeThreewithDistinctCharacters.java
+++ SDET_DSA/src/main/java/mandatoryHomework/week1/TwoPointer_SlidingWindow/SubstringofSizeThreewithDistinctCharacters.java
@@ -1,9 +1,27 @@
 package mandatoryHomework.week1.TwoPointer_SlidingWindow;
 
 public class SubstringofSizeThreewithDistinctCharacters {
-
-	public static void main(String[] args) {
-	
+	/*
+	 * https://leetcode.com/problems/substrings-of-size-three-with-distinct-
+	 * characters/description/
+	 * Input: s = "xyzzaz"
+	 *	Output: 1
+	 *1)Iterate through the string
+	 *2)take i and i+1 , i+2 if all the 3 chars are different then increment count+1
+	 *3)iterate until length of string
+	 */	public static void main(String[] args) {
+		 
+		 String s = "aababcabc";
+		 
+		 int output = 0;
+		 
+		 for (int i = 0; i < s.length()-2; i++) {
+			if (s.charAt(i)!=s.charAt(i+1)&&s.charAt(i+1)!=s.charAt(i+2)&&s.charAt(i)!=s.charAt(i+2)) {
+				output++;
+			}
+		}
+		 
+		 System.out.println(output);
 
 	}
 

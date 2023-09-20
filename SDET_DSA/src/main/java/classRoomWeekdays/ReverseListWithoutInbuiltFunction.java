f8711108462daaae365b6be747ff03924b890ae8
tamilelavenil <tamilelavenil@gmail.com>
2023-09-20 06:38:53 +0530

    Reverse a list two pointers

---
+++ SDET_DSA/src/main/java/classRoomWeekdays/ReverseListWithoutInbuiltFunction.java
@@ -0,0 +1,27 @@
+package classRoomWeekdays;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ReverseListWithoutInbuiltFunction {
+	/* 1)Iterate through the list 
+	 * 2)get left of list in temp
+	 * 3)set left = right and right is equal to left
+	 * 4)left++ right --;
+	 * */
+	public static void main(String[] args) {
+		List<Integer> list = Arrays.asList(1,2,3,4,5);
+		int l =0 , r =list.size()-1;		
+		
+		while(l<r) {
+			int temp = list.get(l);
+			list.set(l,list.get(r));
+			list.set(r, temp);
+			l++; r--;
+		}
+		
+		System.out.println(list);
+
+	}
+
+}

8a01dd0bc0ed52b51aff8df5d10199dc333da78d
tamilelavenil <tamilelavenil@gmail.com>
2023-09-21 11:04:35 +0530

    NumberOfSubstringsContainingAllThreeCharacters

--- SDET_DSA/src/main/java/mandatoryHomework/week1/TwoPointer_SlidingWindow/NumberOfSubstringsContainingAllThreeCharacters.java
+++ SDET_DSA/src/main/java/mandatoryHomework/week1/TwoPointer_SlidingWindow/NumberOfSubstringsContainingAllThreeCharacters.java
@@ -9,9 +9,25 @@ public class NumberOfSubstringsContainingAllThreeCharacters {
 		Explanation: The substrings containing at least one occurrence of the characters a, b and c are "abc", "abca", "abcab", "abcabc", "bca", "bcab",
  		"bcabc", "cab", "cabc" and "abc" (again). 
  		
- 		
+ 		1)Iterate through the string with i=0
+ 		2)inner loop with j=i+2
+ 		3)take substring of i, j+1 (eg: 0,3 - abc)
+ 		4)check for abc and increase count
 	 */	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 String s= "abcabc";
+		 int count = 0;
+	        int len = s.length();
+
+	        for (int i = 0; i < len; i++) {
+	            for (int j = i + 2; j < len; j++) {
+	                String substring = s.substring(i, j + 1);
+	                if (substring.contains("a") && substring.contains("b") && substring.contains("c")) {
+	                    count++;
+	                }
+	            }
+	        }
+
+	       System.out.println(count);
 
 	}
 

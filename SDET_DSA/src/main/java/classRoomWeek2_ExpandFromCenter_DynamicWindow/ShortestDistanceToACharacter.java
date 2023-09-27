996218772d73031877a31d368462447bf9e1e3d5
tamilelavenil <tamilelavenil@gmail.com>
2023-09-27 22:23:37 +0530

    shortest-distance-to-a-character

--- SDET_DSA/src/main/java/classRoomWeek2_ExpandFromCenter_DynamicWindow/ShortestDistanceToACharacter.java
+++ SDET_DSA/src/main/java/classRoomWeek2_ExpandFromCenter_DynamicWindow/ShortestDistanceToACharacter.java
@@ -1,9 +1,50 @@
 package classRoomWeek2_ExpandFromCenter_DynamicWindow;
 
-public class ShortestDistanceToACharacter {
+import java.util.Arrays;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class ShortestDistanceToACharacter {
+	/*
+	 * https://leetcode.com/problems/shortest-distance-to-a-character/description/
+	 *  Input: s = "loveleetcode", c = "e"
+				 Output: [3,2,1,0,1,0,0,1,2,2,1,0]
+	 * 1)Initially keep position as - of string.length
+	 * 2)iterate through the string if the charAt if (i) is not equal to target char = update i-position
+	 * 3)if the charAt(i) matches the target char then update the output array as 0 and make position = i
+	 * 4)repeat the same process 3 and 4 from reverse the string also
+	 * 5)if the i-position is minimum than the previously updated value then update the minimum value in same position
+	 */	public static void main(String[] args) {
+		 String s= "loveleetcode"; char c ='e';
+		 
+		 int[] output = new int[s.length()];
+		 
+		 
+		 int n = s.length(), pos = -n;
+		 
+		 for (int i = 0; i < output.length; i++) {
+			if (s.charAt(i)!=c) {
+				output[i] = i - pos;
+			}
+			if (s.charAt(i)==c) {
+				pos=i;
+				
+				output[i] = i - pos;
+			}
+		 }
+		 System.out.println(Arrays.toString(output));
+		 for (int i = output.length-1; i >=0; i--) {
+			 
+				if (s.charAt(i)!=c) {
+					output[i] = Math.min(output[i], Math.abs(i-pos));
+				}
+				if (s.charAt(i)==c) {
+					pos=i;
+					
+					output[i] = pos-i;
+				}
+			 }
+		 
+		 System.out.println(Arrays.toString(output));
+		 
 
 	}
 
--- SDET_DSA/src/main/java/mandatoryHomework/week1/TwoPointer_SlidingWindow/SortArrayByParityII.java
+++ SDET_DSA/src/main/java/mandatoryHomework/week1/TwoPointer_SlidingWindow/SortArrayByParityII.java
@@ -1,10 +1,39 @@
 package mandatoryHomework.week1.TwoPointer_SlidingWindow;
 
+import java.util.Arrays;
+
 public class SortArrayByParityII {
+	/*
+	 * https://leetcode.com/problems/sort-array-by-parity-ii/description/
+	 * 
+	 * Example 1:
 
+		Input: nums = [4,2,5,7]
+		Output: [4,5,2,7]
+		Explanation: [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been accepted.
+		
+		1)have a output array to return result
+		2)iterate through the nums array take all the odd digits and add it in odd position of out array
+		3)iterate through the nums array take all the even digits and add it in even position of out array
+		4)return the out array
+	 */	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] nums = {4,2,5,7};
+		
+		int out[] = new int[nums.length];
+		int index = 0, index1=1, left=0, right = nums.length-1;
+		
+		 for (int i = 0; i < nums.length; i++) {
+		 	if(nums[i]%2==0&& index<out.length) {
+		 	out[index]= nums[i];
+		 	index+=2;}
+		 		if(nums[i]%2!=0 && index1<out.length) {
+		 	out[index1]= nums[i];
+		 	index1+=2;}
+		 }
+		
 
+		System.out.println(Arrays.toString(out));
 	}
 
 }
